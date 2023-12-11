a2b448d5c286407688b8dbd3b143272e713bb1e7
Syed Shaaf Banoori <SyedShaafBanoori@gmail.com>
2023-12-11 16:52:44 +0500

    TestCases_Added

--- tests/FormElements.spec.js
+++ tests/FormElements.spec.js
@@ -43,9 +43,9 @@ test('Different Styles', async() => {
     await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(3) > input').fill('10')
     await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(4) > input').fill('10')
     //Custom Select
-    await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(6) option:nth-child(1)').click
-    await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(7) option:nth-child(2)').click
-    await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(8) option:nth-child(3)').click
+    // await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(6) option:nth-child(1)').click()
+    // await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(7) option:nth-child(2)').click()
+    // await page.locator('.card.card-primary > div:nth-child(2) > div:nth-child(8) option:nth-child(3)').click()
 })
 
 test('General Elements', async() => {
@@ -63,7 +63,47 @@ test('General Elements', async() => {
     await expect(page.locator('form:nth-child(1) > div:nth-child(6) > div:nth-child(1) > div > div:nth-child(2) > input ')).toBeChecked()
     await expect(page.locator('form:nth-child(1) > div:nth-child(6) > div:nth-child(1) > div > div:nth-child(3) > input ')).toBeDisabled()
     //Radio Buttons
-    expect(await page.locator('form:nth-child(1) > div:nth-child(6) > div:nth-child(2) > div > div:nth-child(1) > input ').isChecked()).toBeFalsy;
-    expect(await page.locator('form:nth-child(1) > div:nth-child(6) > div:nth-child(2) > div > div:nth-child(2) > input ').isChecked()).toBeTruthy;
-    expect (await page.locator('form:nth-child(1) > div:nth-child(6) > div:nth-child(2) > div > div:nth-child(3) > input')).toHaveAttribute('disabled')    
+    expect(await page.locator('form:nth-child(1)>div:nth-child(6)>div:nth-child(2)>div>div:nth-child(1)>input').isChecked()).toBeFalsy;
+    expect(await page.locator('form:nth-child(1)>div:nth-child(6)>div:nth-child(2)>div>div:nth-child(2)>input').isChecked()).toBeTruthy;
+    expect(await page.locator('form:nth-child(1)>div:nth-child(6)>div:nth-child(2)>div>div:nth-child(3)>input')).toHaveAttribute('disabled')    
+    //Select Options
+    await page.selectOption('form>div:nth-child(7)>div:nth-child(1)>div>select', 'option 4') 
+    expect(await page.locator('form>div:nth-child(7)>div:nth-child(2)>div>select')).toBeDisabled()
+    await page.selectOption('form>div:nth-child(8)>div:nth-child(1)>div>select', 'option 3')
+    expect(await page.locator('form>div:nth-child(8)>div:nth-child(2)>div>select')).toBeDisabled()
+})
+
+test('Input Addon', async() => {
+    await page.locator('div:nth-child(3)>div.card-body>div:nth-child(1)>input').fill('TestName')
+    await page.locator('div:nth-child(3)>div.card-body>div:nth-child(2)>input').fill('100')
+    await page.locator('div:nth-child(3)>div.card-body>div:nth-child(3)>input').fill('100')
+    //With Icons
+    await page.locator('div:nth-child(3)>div>div:nth-child(5)>input').fill('Test@gmail.com')
+    await page.locator('div:nth-child(3)>div>div:nth-child(6)>input').fill('Correct')
+    await page.locator('div:nth-child(3)>div>div:nth-child(7)>input').fill('100')
+    //With Checkbox and Radio Inputs
+    await page.locator('div:nth-child(3)>div>div>div:nth-child(1)>div>div>span>input').check()
+    await page.locator('div:nth-child(3)>div>div>div:nth-child(1)>div>input').fill('Checkbox')
+    await page.locator('div:nth-child(3)>div>div>div:nth-child(2)>div>div>span>input').click()
+    await page.locator('div:nth-child(3)>div>div>div:nth-child(2)>div>input').fill('Radio Button')
+    //With Buttons
+    await page.locator('.input-group.input-group-lg.mb-3>div>button').click()
+    await page.locator('.input-group.input-group-lg.mb-3>div>ul').click()
+    await page.locator('.input-group.input-group-lg.mb-3>input').fill('Action')
+    await page.locator('div:nth-child(3)>div>div:nth-child(14)>input').fill('Action')
+    await page.locator('div:nth-child(3)>div>div.input-group.input-group-sm>input').fill('Action')
+    await page.locator('.input-group.input-group-sm>span>button').click()
+})
+
+test('Custom Elements', async() => {
+    //Checkboxes
+    // expect(await page.locator('.col-md-6>div:nth-child(4)>div:nth-child(2)>form>div>div:nth-child(1)>div>div:nth-child(1)>#customCheckbox1')).not.toBeChecked()
+    // expect(await page.locator('.col-md-6>div:nth-child(4)>div:nth-child(2)>form>div>div:nth-child(1)>div>div:nth-child(2)>#customCheckbox2')).toBeChecked()
+    // expect(await page.locator('.col-md-6>div:nth-child(4)>div:nth-child(2)>form>div>div:nth-child(1)>div>div:nth-child(3)>#customCheckbox3')).toBeDisabled()
+    // expect(await page.locator('form>div>div:nth-child(1)>div>div:nth-child(4)>input')).toBeChecked()
+    // expect(await page.locator('form>div>div:nth-child(1)>div>div:nth-child(5)>input')).toBeChecked()
+    //Radio Buttons
+    // expect(await page.locator('').isChecked()).toBeFalsy;
+    // expect(await page.locator('').isChecked()).toBeTruthy;
+    // expect(await page.locator('')).toHaveAttribute('disabled')
 })
